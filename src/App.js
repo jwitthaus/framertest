bfb376a4e58e97448540afcea6c3f17da3143f1d
jwitthaus <joerg.witthaus@gmail.com>
2024-06-10 12:42:26 +0200

    easy example for sandbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Paper } from "@mui/material";
+import "./App.css";
+import { useState } from "react";
+import { animate, motion } from "framer-motion";
 
 function App() {
+  const [zoom, setZoom] = useState(10);
+
+  const AnimatedPaper = motion(Paper);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        variant="contained"
+        onClick={() => {
+          setZoom(zoom + 10);
+        }}
+      >
+        increase size
+      </Button>
+      <AnimatedPaper
+        sx={{
+          height: "100px",
+          backgroundColor: "#aaaaaa",
+        }}
+        animate={{ width: `${zoom}%` }}
+      ></AnimatedPaper>
     </div>
   );
 }
